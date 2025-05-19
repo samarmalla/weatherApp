120d5bc7123dcdf7692e70c118f4cd3071719de6
samarmalla <samar.malla1@gmail.com>
2025-05-19 12:46:28 -0700

    javascript api keys initalized

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function getWeather(){
 
+    const apiKey = "9b68baccd0d459e81f3b34aee3608c55";
+    const city = document.getElementById("city").value;
+}
+
+if(!city){
+    alert ("Please enter a city");
+    return;
+}
